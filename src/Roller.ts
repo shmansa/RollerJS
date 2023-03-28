3b5aef8c5a3accf36227f69ba25e1451261de479
shmansa <124243497+shmansa@users.noreply.github.com>
2023-03-28 09:51:06 -0700

    Implement function in Roller.ts

--- src/Roller.ts
+++ src/Roller.ts
@@ -7,7 +7,12 @@ export class Roller {
     // so any invalid values should default to 6 to represent the common 
     // six-sided die.
     constructor(faces: number) {
-        faces;
+        this._faces = faces >= 2 ? faces : 6;
+        this._distribution = new Map<number, number>();
+        for (let i = 1; i <= this._faces; i++) {
+            this._distribution.set(i, 0);
+        }
+        this._last = 0;
     }
 
     // specifies which number should be rolled and returns the recorded roll. 
@@ -20,14 +25,19 @@ export class Roller {
     //   3. the distribution should update the distribution record so that the
     //      key (value rolled) has an updated value that increases the number
     //      of rolls for that face by 1
-    roll(value: number): number{
+    roll(value: number): number {
+        if (value < 1 || value > this._faces) {
+            return 0;
+        }
+        this._last = value;
+        this._distribution.set(value, this._distribution.get(value)! + 1);
         return value;
     }
 
     // returns the value of the latest die roll. If no rolls have been made
     // yet, then it should return 0.
     last(): number {
-        return 0;
+        return this._last;
     }
 
     // returns a Map that represents the current distribution of rolls for each
@@ -35,7 +45,7 @@ export class Roller {
     // value and its value should be the number of times that face has been
     // rolled. Even if a face has not yet been rolled, its key should have a
     // value of 0.
-    distribution(): Map<number, number>{
-        return new Map<number, number>();
+    distribution(): Map<number, number> {
+        return new Map<number, number>(this._distribution);
     }
 } // end of Roller class
